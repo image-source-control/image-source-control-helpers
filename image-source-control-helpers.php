cd9715cb9c9f70d046fd5eb1524482edd0e62358
Thomas Maier <post@webzunft.de>
2023-12-06 17:04:04 +0100

    more helper functions
    - function to disable ISC on non-posts
    
    - change caption markup into an icon

--- image-source-control-helpers.php
+++ image-source-control-helpers.php
@@ -80,3 +80,75 @@ function isc_helpers_log_update_isc_image_source_post_meta( $meta_id, $post_id, 
 	error_log( $message );
 };
 add_action( 'update_post_meta', 'isc_helpers_log_update_isc_image_source_post_meta', 10, 4 );
+
+/**
+ * Change the caption markup into an icon that extends on hover
+ *
+ * @param string $source_html HTML of the image source overlay.
+ *
+ * @return string
+ */
+function isc_helpers_change_caption_markup( string $source_html ): string {
+
+	return sprintf(
+		'<span class="isc-source-text"><i class="isc-source-text-icon">©</i><span>%s</span></span>',
+		// remove existing HTML wrapper and only keep links
+		wp_kses(
+			$source_html,
+			[
+				'a' => [
+					'href'   => [],
+					'target' => [],
+					'rel'    => [],
+				],
+			]
+		)
+	);
+}
+add_filter( 'isc_overlay_html_source', 'isc_helpers_change_caption_markup' );
+
+/**
+ * Add custom CSS to site header to show the caption when hovering over the icon
+ */
+function isc_helpers_change_caption_markup_header_css() {
+	echo '<style>
+        .isc-source-text {
+            position: relative;
+            display: inline-block;
+            cursor: help;
+        }
+
+        .isc-source-text .isc-source-text-icon {
+            font-size: 2em;
+        }
+
+        .isc-source-text > span {
+            position: absolute;
+            bottom: 100%;
+            left: 50%;
+            transform: translateX(-50%);
+            visibility: hidden;
+            opacity: 0;
+            transition: opacity 0.3s ease, visibility 0.3s ease;
+            padding: 5px;
+            border: 1px solid #ccc;
+        }
+
+        .isc-source-text:hover > span {
+            visibility: visible;
+            opacity: 1;
+        }
+    </style>';
+}
+add_action('wp_head', 'isc_helpers_change_caption_markup_header_css');
+
+/**
+ * Disable all ISC functionality for non-post pages
+ */
+function isc_helpers_disable_isc_on_non_posts( $post_ids ) {
+	if ( ! is_single() ) {
+		$post_ids[] = get_the_ID();
+	}
+	return $post_ids;
+}
+add_filter( 'isc_public_excluded_post_ids', 'isc_helpers_disable_isc_on_non_posts' );
